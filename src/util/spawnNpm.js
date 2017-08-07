243f2f89002d4dc68715e8564ed8a326de18944a
Cristóvão Honorato <cristovao.honorato@gmail.com>
2017-08-07 13:53:22 +0200

    Correcting npm binary location

--- src/util/spawnNpm.js
+++ src/util/spawnNpm.js
@@ -1,5 +1,6 @@
-import { spawn } from '../util/childProcess'
 import path from 'path'
+
+import { spawn } from '../util/childProcess'
 
 export default function(args, options, callback) {
     return isGradleRun()
@@ -24,10 +25,12 @@ function fromBinaries(args, options, callback) {
 
 function findNpmPath() {
     const parts = process.execPath.split(path.sep)
-    const basePath = parts.slice(0, parts.indexOf('.gradle') + 1).join(path.sep)
+
+    // +3 cuz it is ./gradle/nodejs/<version>/lib/node_modules/npm/bin/npm-cli.js
+    const basePath = parts.slice(0, parts.indexOf('.gradle') + 3).join(path.sep)
     return path.join(
         basePath,
-        'npm',
+        'lib',
         'node_modules',
         'npm',
         'bin',
