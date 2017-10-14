3ad2fff655e0321812a23ec540b9bfc94c1c9f1e
Cristóvão Honorato <cristovao.honorato@gmail.com>
2017-10-14 21:20:41 +0200

    Extra tests

--- test/lock-file/patchOptional.js
+++ test/lock-file/patchOptional.js
@@ -4,11 +4,23 @@ import patchOptional from '../../src/lock-file/patchOptional'
 import packageLockMac from '../fixtures/package-lock-mac.json'
 import packageLockLinux from '../fixtures/package-lock-linux.json'
 
+import packageLockSignavioJsMac from '../fixtures/plock-signavio-js-mac.json'
+import packageLockSignavioJsWin from '../fixtures/plock-signavio-js-windows.json'
+
 describe('lock-file', () => {
     describe('patchOptional', () => {
         it('Should patch MAC to LINUX', () => {
             const expected = packageLockMac
             const result = patchOptional(packageLockMac, packageLockLinux)
+            expect(expected).to.deep.equal(result)
+        })
+
+        it('Should patch MAC to WINDOWS', () => {
+            const expected = packageLockSignavioJsMac
+            const result = patchOptional(
+                packageLockSignavioJsMac,
+                packageLockSignavioJsWin
+            )
             expect(expected).to.deep.equal(result)
         })
     })
