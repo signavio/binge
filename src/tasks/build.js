418fa9085d103550277bf4b71823526138e0373f
Cristóvão Honorato <cristovao.honorato@gmail.com>
2017-10-17 12:57:02 +0200

    Lint error

--- src/tasks/build.js
+++ src/tasks/build.js
@@ -1,13 +1,10 @@
-//import exec from '../util/exec'
+// import exec from '../util/exec'
 import spawn from '../util/spawn'
 
 export default function(node, entryNode, callback) {
     if (node.isDummy === true || node.isApp === true || node === entryNode) {
         return callback(null)
     }
-    const unavailable =
-        node.packageJson.scripts && !node.packageJson.scripts.build
-
     const options = {
         cwd: node.path,
         stdio: 'inherit',
