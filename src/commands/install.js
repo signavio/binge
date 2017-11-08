66cd9252318ec6bda61bb0fb39b63475b8cae1cb
Cristóvão Honorato <cristovao.honorato@gmail.com>
2017-11-08 12:55:47 +0100

    Correct install for single nodes

--- src/commands/install.js
+++ src/commands/install.js
@@ -17,7 +17,9 @@ export default function(cliFlags) {
         })
 
         if (nodes.length === 1) {
-            taskInstall(nodes[0], end)
+            taskInstall(nodes[0], (err, result) => {
+                end(err, [result])
+            })
             return
         }
 
